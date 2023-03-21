2cd11f7a80372ac6347b9cbe4ed36ed7b30bfb4e
Ryan Kwok <ryankwok123@hotmail.com>
2023-03-20 21:24:03 -0400

    cleaning up again

--- lunar2gregorian.py
+++ lunar2gregorian.py
@@ -102,39 +102,14 @@ class to_dataframe(convert_date):
         return 'th' if 11<=d<=13 else {1:'st',2:'nd',3:'rd'}.get(d%10, 'th')
         
 
-<<<<<<< HEAD
-    #appending columns to our dataframe
-    df['Subject'] = [f'{name}s birthday' for subject in range(len(birthdates))]
-
-    df['Start Date'] = birthdates
-    # Use pandas.to_datetime() to convert string to datetime format (only keeping date portion)
-    df["Start Date"] = pd.to_datetime(df["Start Date"]).dt.date 
-
-    df['Start Time'] = ['9:00:00' for starttime in range(len(birthdates))]  
-
-    #display different description message based on whether or not the birthday lands on a leap month
-    def description():
-
-        #short function that attaches the appropriate suffix based on the number    
-        def suffix(d):
-            return 'th' if 11<=d<=13 else {1:'st',2:'nd',3:'rd'}.get(d%10, 'th')
-=======
     #display different description message based on whether or not the birthday lands on a leap month    
     def append_description(self):
         df = to_dataframe.append_subject_startdate_starttime(self)
->>>>>>> OOP
 
         #messages for regular birthdays and leap month birthdays
         birthdate_year = pd.to_datetime(df["Start Date"]).dt.year #df[Start Date] that only contains the year portion
 
         desc_list = []
-<<<<<<< HEAD
-        for index in range(len(valid_list)):
-            if valid_list[index]==False:
-                desc_list.append(f'Its {name}s birthday today in the chinese lunar calendar ({birthdate_year.iloc[index]}, {month}{suffix(month)} month, {day}{suffix(day)} day)!')
-            else:
-                desc_list.append(f'Its {name}s birthday today in the chinese lunar calendar ({birthdate_year.iloc[index]}, {month}{suffix(month)} month, {day}{suffix(day)} day)! This is one of two birthdays {name} will have this year since their birthday lands on a leap month.')  
-=======
         for index in range(len(self.valid_list_with_repeats)):
             if self.valid_list_with_repeats[index]==False:
                 desc_list.append(f'Its {self.name}s birthday today in the chinese lunar calendar ({birthdate_year.iloc[index]}, {self.month}{self.suffix(self.month)} month, {self.day}{self.suffix(self.day)} day)!')
@@ -143,22 +118,14 @@ class to_dataframe(convert_date):
         
         df['Description'] = desc_list
         return df
->>>>>>> OOP
         
     
     def display_and_save_to_csv(self):
         df = to_dataframe.append_description(self)
         column_names = ['Subject','Start Date', 'Start Time','Description']
 
-<<<<<<< HEAD
-    
-    
-    #sample table
-    print(tabulate(df, headers=column_names, tablefmt='psql'))
-=======
         #sample table
         print(tabulate(df, headers=column_names, tablefmt='psql'))
->>>>>>> OOP
 
         #save as csv
         df.to_csv('lunar_to_gregorian_birthdays.csv', index = False)
